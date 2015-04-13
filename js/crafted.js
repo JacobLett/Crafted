368e6aeb60e65b442be0e76ae4d7b87ee3335512
Jacob Lett <jacoblett@bootstrapcreative.com>
2015-04-13 06:33:38 -0400

    Theme updates
    - enable Style Editor
    - yellow rating stars
    - homepage carousel autostart

--- js/crafted.js
+++ js/crafted.js
@@ -68,7 +68,7 @@ if (!$("html").hasClass("lt-ie9")) {
 	 *	http://en.wikipedia.org/wiki/MIT_License
 	 *	http://en.wikipedia.org/wiki/GNU_General_Public_License
 	 */
-	!function(t,e){function n(t,e,n){var r=t.children(),o=!1;t.empty();for(var i=0,d=r.length;d>i;i++){var l=r.eq(i);if(t.append(l),n&&t.append(n),a(t,e)){l.remove(),o=!0;break}n&&n.detach()}return o}function r(e,n,i,d,l){var s=!1,c="table, thead, tbody, tfoot, tr, col, colgroup, object, embed, param, ol, ul, dl, blockquote, select, optgroup, option, textarea, script, style",u="script, .dotdotdot-keep";return e.contents().detach().each(function(){var f=this,h=t(f);if("undefined"==typeof f||3==f.nodeType&&0==t.trim(f.data).length)return!0;if(h.is(u))e.append(h);else{if(s)return!0;e.append(h),l&&e[e.is(c)?"after":"append"](l),a(i,d)&&(s=3==f.nodeType?o(h,n,i,d,l):r(h,n,i,d,l),s||(h.detach(),s=!0)),s||l&&l.detach()}}),s}function o(e,n,r,o,d){var c=e[0];if(!c)return!1;var f=s(c),h=-1!==f.indexOf(" ")?" ":"　",p="letter"==o.wrap?"":h,g=f.split(p),v=-1,w=-1,b=0,y=g.length-1;for(o.fallbackToLetter&&0==b&&0==y&&(p="",g=f.split(p),y=g.length-1);y>=b&&(0!=b||0!=y);){var m=Math.floor((b+y)/2);if(m==w)break;w=m,l(c,g.slice(0,w+1).join(p)+o.ellipsis),a(r,o)?(y=w,o.fallbackToLetter&&0==b&&0==y&&(p="",g=g[0].split(p),v=-1,w=-1,b=0,y=g.length-1)):(v=w,b=w)}if(-1==v||1==g.length&&0==g[0].length){var x=e.parent();e.detach();var T=d&&d.closest(x).length?d.length:0;x.contents().length>T?c=u(x.contents().eq(-1-T),n):(c=u(x,n,!0),T||x.detach()),c&&(f=i(s(c),o),l(c,f),T&&d&&t(c).parent().append(d))}else f=i(g.slice(0,v+1).join(p),o),l(c,f);return!0}function a(t,e){return t.innerHeight()>e.maxHeight}function i(e,n){for(;t.inArray(e.slice(-1),n.lastCharacter.remove)>-1;)e=e.slice(0,-1);return t.inArray(e.slice(-1),n.lastCharacter.noEllipsis)<0&&(e+=n.ellipsis),e}function d(t){return{width:t.innerWidth(),height:t.innerHeight()}}function l(t,e){t.innerText?t.innerText=e:t.nodeValue?t.nodeValue=e:t.textContent&&(t.textContent=e)}function s(t){return t.innerText?t.innerText:t.nodeValue?t.nodeValue:t.textContent?t.textContent:""}function c(t){do t=t.previousSibling;while(t&&1!==t.nodeType&&3!==t.nodeType);return t}function u(e,n,r){var o,a=e&&e[0];if(a){if(!r){if(3===a.nodeType)return a;if(t.trim(e.text()))return u(e.contents().last(),n)}for(o=c(a);!o;){if(e=e.parent(),e.is(n)||!e.length)return!1;o=c(e[0])}if(o)return u(t(o),n)}return!1}function f(e,n){return e?"string"==typeof e?(e=t(e,n),e.length?e:!1):e.jquery?e:!1:!1}function h(t){for(var e=t.innerHeight(),n=["paddingTop","paddingBottom"],r=0,o=n.length;o>r;r++){var a=parseInt(t.css(n[r]),10);isNaN(a)&&(a=0),e-=a}return e}if(!t.fn.dotdotdot){t.fn.dotdotdot=function(e){if(0==this.length)return t.fn.dotdotdot.debug('No element found for "'+this.selector+'".'),this;if(this.length>1)return this.each(function(){t(this).dotdotdot(e)});var o=this;o.data("dotdotdot")&&o.trigger("destroy.dot"),o.data("dotdotdot-style",o.attr("style")||""),o.css("word-wrap","break-word"),"nowrap"===o.css("white-space")&&o.css("white-space","normal"),o.bind_events=function(){return o.bind("update.dot",function(e,d){e.preventDefault(),e.stopPropagation(),l.maxHeight="number"==typeof l.height?l.height:h(o),l.maxHeight+=l.tolerance,"undefined"!=typeof d&&(("string"==typeof d||d instanceof HTMLElement)&&(d=t("<div />").append(d).contents()),d instanceof t&&(i=d)),g=o.wrapInner('<div class="dotdotdot" />').children(),g.contents().detach().end().append(i.clone(!0)).find("br").replaceWith("  <br />  ").end().css({height:"auto",width:"auto",border:"none",padding:0,margin:0});var c=!1,u=!1;return s.afterElement&&(c=s.afterElement.clone(!0),c.show(),s.afterElement.detach()),a(g,l)&&(u="children"==l.wrap?n(g,l,c):r(g,o,g,l,c)),g.replaceWith(g.contents()),g=null,t.isFunction(l.callback)&&l.callback.call(o[0],u,i),s.isTruncated=u,u}).bind("isTruncated.dot",function(t,e){return t.preventDefault(),t.stopPropagation(),"function"==typeof e&&e.call(o[0],s.isTruncated),s.isTruncated}).bind("originalContent.dot",function(t,e){return t.preventDefault(),t.stopPropagation(),"function"==typeof e&&e.call(o[0],i),i}).bind("destroy.dot",function(t){t.preventDefault(),t.stopPropagation(),o.unwatch().unbind_events().contents().detach().end().append(i).attr("style",o.data("dotdotdot-style")||"").data("dotdotdot",!1)}),o},o.unbind_events=function(){return o.unbind(".dot"),o},o.watch=function(){if(o.unwatch(),"window"==l.watch){var e=t(window),n=e.width(),r=e.height();e.bind("resize.dot"+s.dotId,function(){n==e.width()&&r==e.height()&&l.windowResizeFix||(n=e.width(),r=e.height(),u&&clearInterval(u),u=setTimeout(function(){o.trigger("update.dot")},100))})}else c=d(o),u=setInterval(function(){if(o.is(":visible")){var t=d(o);(c.width!=t.width||c.height!=t.height)&&(o.trigger("update.dot"),c=t)}},500);return o},o.unwatch=function(){return t(window).unbind("resize.dot"+s.dotId),u&&clearInterval(u),o};var i=o.contents(),l=t.extend(!0,{},t.fn.dotdotdot.defaults,e),s={},c={},u=null,g=null;return l.lastCharacter.remove instanceof Array||(l.lastCharacter.remove=t.fn.dotdotdot.defaultArrays.lastCharacter.remove),l.lastCharacter.noEllipsis instanceof Array||(l.lastCharacter.noEllipsis=t.fn.dotdotdot.defaultArrays.lastCharacter.noEllipsis),s.afterElement=f(l.after,o),s.isTruncated=!1,s.dotId=p++,o.data("dotdotdot",!0).bind_events().trigger("update.dot"),l.watch&&o.watch(),o},t.fn.dotdotdot.defaults={ellipsis:"... ",wrap:"word",fallbackToLetter:!0,lastCharacter:{},tolerance:0,callback:null,after:null,height:null,watch:!1,windowResizeFix:!0},t.fn.dotdotdot.defaultArrays={lastCharacter:{remove:[" ","　",",",";",".","!","?"],noEllipsis:[]}},t.fn.dotdotdot.debug=function(){};var p=1,g=t.fn.html;t.fn.html=function(n){return n!=e&&!t.isFunction(n)&&this.data("dotdotdot")?this.trigger("update",[n]):g.apply(this,arguments)};var v=t.fn.text;t.fn.text=function(n){return n!=e&&!t.isFunction(n)&&this.data("dotdotdot")?(n=t("<div />").text(n).html(),this.trigger("update",[n])):v.apply(this,arguments)}}}(jQuery);
+	!function(t,e){function n(t,e,n){var r=t.children(),o=!1;t.empty();for(var i=0,d=r.length;d>i;i++){var l=r.eq(i);if(t.append(l),n&&t.append(n),a(t,e)){l.remove(),o=!0;break}n&&n.detach()}return o}function r(e,n,i,d,l){var s=!1,c="table, thead, tbody, tfoot, tr, col, colgroup, object, embed, param, ol, ul, dl, blockquote, select, optgroup, option, textarea, script, style",u="script, .dotdotdot-keep";return e.contents().detach().each(function(){var f=this,h=t(f);if("undefined"==typeof f||3==f.nodeType&&0==t.trim(f.data).length)return!0;if(h.is(u))e.append(h);else{if(s)return!0;e.append(h),l&&e[e.is(c)?"after":"append"](l),a(i,d)&&(s=3==f.nodeType?o(h,n,i,d,l):r(h,n,i,d,l),s||(h.detach(),s=!0)),s||l&&l.detach()}}),s}function o(e,n,r,o,d){var c=e[0];if(!c)return!1;var f=s(c),h=-1!==f.indexOf(" ")?" ":"Ã£â‚¬â‚¬",p="letter"==o.wrap?"":h,g=f.split(p),v=-1,w=-1,b=0,y=g.length-1;for(o.fallbackToLetter&&0==b&&0==y&&(p="",g=f.split(p),y=g.length-1);y>=b&&(0!=b||0!=y);){var m=Math.floor((b+y)/2);if(m==w)break;w=m,l(c,g.slice(0,w+1).join(p)+o.ellipsis),a(r,o)?(y=w,o.fallbackToLetter&&0==b&&0==y&&(p="",g=g[0].split(p),v=-1,w=-1,b=0,y=g.length-1)):(v=w,b=w)}if(-1==v||1==g.length&&0==g[0].length){var x=e.parent();e.detach();var T=d&&d.closest(x).length?d.length:0;x.contents().length>T?c=u(x.contents().eq(-1-T),n):(c=u(x,n,!0),T||x.detach()),c&&(f=i(s(c),o),l(c,f),T&&d&&t(c).parent().append(d))}else f=i(g.slice(0,v+1).join(p),o),l(c,f);return!0}function a(t,e){return t.innerHeight()>e.maxHeight}function i(e,n){for(;t.inArray(e.slice(-1),n.lastCharacter.remove)>-1;)e=e.slice(0,-1);return t.inArray(e.slice(-1),n.lastCharacter.noEllipsis)<0&&(e+=n.ellipsis),e}function d(t){return{width:t.innerWidth(),height:t.innerHeight()}}function l(t,e){t.innerText?t.innerText=e:t.nodeValue?t.nodeValue=e:t.textContent&&(t.textContent=e)}function s(t){return t.innerText?t.innerText:t.nodeValue?t.nodeValue:t.textContent?t.textContent:""}function c(t){do t=t.previousSibling;while(t&&1!==t.nodeType&&3!==t.nodeType);return t}function u(e,n,r){var o,a=e&&e[0];if(a){if(!r){if(3===a.nodeType)return a;if(t.trim(e.text()))return u(e.contents().last(),n)}for(o=c(a);!o;){if(e=e.parent(),e.is(n)||!e.length)return!1;o=c(e[0])}if(o)return u(t(o),n)}return!1}function f(e,n){return e?"string"==typeof e?(e=t(e,n),e.length?e:!1):e.jquery?e:!1:!1}function h(t){for(var e=t.innerHeight(),n=["paddingTop","paddingBottom"],r=0,o=n.length;o>r;r++){var a=parseInt(t.css(n[r]),10);isNaN(a)&&(a=0),e-=a}return e}if(!t.fn.dotdotdot){t.fn.dotdotdot=function(e){if(0==this.length)return t.fn.dotdotdot.debug('No element found for "'+this.selector+'".'),this;if(this.length>1)return this.each(function(){t(this).dotdotdot(e)});var o=this;o.data("dotdotdot")&&o.trigger("destroy.dot"),o.data("dotdotdot-style",o.attr("style")||""),o.css("word-wrap","break-word"),"nowrap"===o.css("white-space")&&o.css("white-space","normal"),o.bind_events=function(){return o.bind("update.dot",function(e,d){e.preventDefault(),e.stopPropagation(),l.maxHeight="number"==typeof l.height?l.height:h(o),l.maxHeight+=l.tolerance,"undefined"!=typeof d&&(("string"==typeof d||d instanceof HTMLElement)&&(d=t("<div />").append(d).contents()),d instanceof t&&(i=d)),g=o.wrapInner('<div class="dotdotdot" />').children(),g.contents().detach().end().append(i.clone(!0)).find("br").replaceWith("  <br />  ").end().css({height:"auto",width:"auto",border:"none",padding:0,margin:0});var c=!1,u=!1;return s.afterElement&&(c=s.afterElement.clone(!0),c.show(),s.afterElement.detach()),a(g,l)&&(u="children"==l.wrap?n(g,l,c):r(g,o,g,l,c)),g.replaceWith(g.contents()),g=null,t.isFunction(l.callback)&&l.callback.call(o[0],u,i),s.isTruncated=u,u}).bind("isTruncated.dot",function(t,e){return t.preventDefault(),t.stopPropagation(),"function"==typeof e&&e.call(o[0],s.isTruncated),s.isTruncated}).bind("originalContent.dot",function(t,e){return t.preventDefault(),t.stopPropagation(),"function"==typeof e&&e.call(o[0],i),i}).bind("destroy.dot",function(t){t.preventDefault(),t.stopPropagation(),o.unwatch().unbind_events().contents().detach().end().append(i).attr("style",o.data("dotdotdot-style")||"").data("dotdotdot",!1)}),o},o.unbind_events=function(){return o.unbind(".dot"),o},o.watch=function(){if(o.unwatch(),"window"==l.watch){var e=t(window),n=e.width(),r=e.height();e.bind("resize.dot"+s.dotId,function(){n==e.width()&&r==e.height()&&l.windowResizeFix||(n=e.width(),r=e.height(),u&&clearInterval(u),u=setTimeout(function(){o.trigger("update.dot")},100))})}else c=d(o),u=setInterval(function(){if(o.is(":visible")){var t=d(o);(c.width!=t.width||c.height!=t.height)&&(o.trigger("update.dot"),c=t)}},500);return o},o.unwatch=function(){return t(window).unbind("resize.dot"+s.dotId),u&&clearInterval(u),o};var i=o.contents(),l=t.extend(!0,{},t.fn.dotdotdot.defaults,e),s={},c={},u=null,g=null;return l.lastCharacter.remove instanceof Array||(l.lastCharacter.remove=t.fn.dotdotdot.defaultArrays.lastCharacter.remove),l.lastCharacter.noEllipsis instanceof Array||(l.lastCharacter.noEllipsis=t.fn.dotdotdot.defaultArrays.lastCharacter.noEllipsis),s.afterElement=f(l.after,o),s.isTruncated=!1,s.dotId=p++,o.data("dotdotdot",!0).bind_events().trigger("update.dot"),l.watch&&o.watch(),o},t.fn.dotdotdot.defaults={ellipsis:"... ",wrap:"word",fallbackToLetter:!0,lastCharacter:{},tolerance:0,callback:null,after:null,height:null,watch:!1,windowResizeFix:!0},t.fn.dotdotdot.defaultArrays={lastCharacter:{remove:[" ","Ã£â‚¬â‚¬",",",";",".","!","?"],noEllipsis:[]}},t.fn.dotdotdot.debug=function(){};var p=1,g=t.fn.html;t.fn.html=function(n){return n!=e&&!t.isFunction(n)&&this.data("dotdotdot")?this.trigger("update",[n]):g.apply(this,arguments)};var v=t.fn.text;t.fn.text=function(n){return n!=e&&!t.isFunction(n)&&this.data("dotdotdot")?(n=t("<div />").text(n).html(),this.trigger("update",[n])):v.apply(this,arguments)}}}(jQuery);
 
 	$("#home .ProductList .ProductDetails a, #CategoryContent .ProductList li:not('.ListView') .ProductDetails a").dotdotdot({
 			/*	The HTML to add as ellipsis. */
@@ -264,8 +264,10 @@ if (!$("html").hasClass("lt-ie9")) {
 		})(jQuery);
 
 		// Activate Plugin - also in window resize
-		 // $('.ProductList .ProductImage img').centerImage();
+		// $('.ProductList .ProductImage img')
+		// 
 		
+	
 		
 }
 // no ie		
@@ -397,23 +399,21 @@ $(".footer #BlogRecentPosts h2").replaceWith('<h4><a href="/blog/">Blog</a></h4>
 
 	
 	// Begin Carousels and sliders
+	// http://owlgraphic.com/owlcarousel/
 		  $("#featureSlide").owlCarousel({
-
-		      navigation : true, // Show next and prev buttons
-		      slideSpeed : 300,
-		      paginationSpeed : 400,
-				autoPlay: false,
+		      	navigation : true, // Show next and prev buttons
+		      	slideSpeed : 600,
+		      	paginationSpeed : 400,
+				// Change to any integrer for example autoPlay : 5000 to play every 5 seconds. If you set autoPlay: true default speed will be 5 seconds.
+				autoPlay: 5000,
+				rewindSpeed: 1,
 				stopOnHover: true,
-		      singleItem:true
-
-		      // "singleItem:true" is a shortcut for:
-		      // items : 1, 
-		      // itemsDesktop : false,
-		      // itemsDesktopSmall : false,
-		      // itemsTablet: false,
-		      // itemsMobile : false
-
+		      	singleItem:true
 		  });
+
+
+
+
 
 
 		$(".productSlide .ProductList").addClass("owl-carousel owl-theme");
@@ -559,22 +559,22 @@ $(".footer #BlogRecentPosts h2").replaceWith('<h4><a href="/blog/">Blog</a></h4>
 	$('.slideText').height(maxHslide).width(maxWslide);
 	
 	
-	// Restructures the homepage tile banner
-	var homeBannerLink = $(".banner_home_page_top a").attr( "href" );
-	var categoryBannerLink = $(".banner_category_page_top a").attr( "href" );
-	var brandBannerLink = $(".banner_brand_page_top a").attr( "href" );
-	var searchBannerLink = $(".banner_search_page_top a").attr( "href" );
-	$(".banner_home_page_top a, .banner_category_page_top a, .banner_brands_page_top a, .banner_search_page_top a").replaceWith(function(){
-	        return $("<span class=cta>" + $(this).html() + "</span>");
-	});
-	$(".banner_home_page_top h1, .banner_category_page_top h1, .banner_brands_page_top h1, .banner_search_page_top h1").replaceWith(function(){
-	        return $("<span class=title>" + $(this).html() + "</span>");
-	});
+//	Restructures the homepage tile banner
+//	var homeBannerLink = $(".banner_home_page_top a").attr( "href" );
+//	var categoryBannerLink = $(".banner_category_page_top a").attr( "href" );
+//	var brandBannerLink = $(".banner_brand_page_top a").attr( "href" );
+//	var searchBannerLink = $(".banner_search_page_top a").attr( "href" );
+//	$(".banner_home_page_top a, .banner_category_page_top a, .banner_brands_page_top a, .banner_search_page_top a").replaceWith(function(){
+//	        return $("<span class=cta>" + $(this).html() + "</span>");
+//	});
+//	$(".banner_home_page_top h1, .banner_category_page_top h1, .banner_brands_page_top h1, .banner_search_page_top h1").replaceWith(function(){
+//        return $("<span class=title>" + $(this).html() + "</span>");
+//	});
 	
-	$(".banner_home_page_top").wrap( '<a href=' + homeBannerLink + '></a>' );
-	$(".banner_category_page_top").wrap( '<a href=' + categoryBannerLink + '></a>' );
-	$(".banner_brand_page_top").wrap( '<a href=' + brandBannerLink + '></a>' );
-	$(".banner_search_page_top").wrap( '<a href=' + searchBannerLink + '></a>' );
+//	$(".banner_home_page_top").wrap( '<a href=' + homeBannerLink + '></a>' );
+//	$(".banner_category_page_top").wrap( '<a href=' + categoryBannerLink + '></a>' );
+//	$(".banner_brand_page_top").wrap( '<a href=' + brandBannerLink + '></a>' );
+//	$(".banner_search_page_top").wrap( '<a href=' + searchBannerLink + '></a>' );
 	
 	
 // Blocks script from running on ie8 and lower
@@ -601,22 +601,42 @@ if (!$("html").hasClass("lt-ie9")) {
 		$('.ProductPriceRating img[src*="Rating0"]').hide();
 	  }
 	
-	// Adds sale tag to product list
+	
+	
+	// Adds sale tag to product list if on sale
 	if ($('.ProductList .SalePrice').length) { // implies *not* zero
-	    //log('We found img elements on the page using "img"');
+		console.log('on sale so add sale tag to product list');
 		$('.ProductList .SalePrice').append( "<span class='saleTag'>Sale</span>" );
 	  }
-	// Adds sale tag to product list
-	if ($('.productPage .p-price .s-price').length) { // implies *not* zero
-	    //log('We found img elements on the page using "img"');
+	
+	// Checks to see if it is not on sale and pre-order
+	if (!$('.ProductList .p-price .SalePrice').length) { // implies exists
+		console.log('');
+		$('.ProductList .p-price strike').replaceWith('<span class="preorder">Pre-Order</span>');
+	  }
+	
+	
+	
+	// Adds sale tag to product page
+	if ($('.productPage .p-price .s-price:contains("save")').length) { // implies *not* zero
+		console.log('product for sale so show sale tag');
 		$('.ProductThumbImage').append( "<span class='saleTag'>Sale</span>" );
 	  }
 	
+	// If product is pre-order do remove the sale price
+	if ($('.productPage .ProductMain .ExpectedReleaseDate').length) { // implies *not* zero
+		console.log('presale so remove sale tag');
+		$('.ProductThumbImage .saleTag').remove();
+		$('.ProductMain .p-price .YouSave').remove();
+		$('.ProductMain .p-price strike').replaceWith('<span class="preorder">Pre-Order</span>');
+	  }
+		
+
 	
 	// Adds a Sold Out or out of stock tag to the product listing and product page
 	if(!$('.ProductList .list-item .ProductActionAdd[style="display:none;"], .ProductList li .ProductActionAdd[style="display:none;"], .ProductList .list-item .ProductActionAdd[style="display: none;"], .ProductList li .ProductActionAdd[style="display: none;"]').length == 0)
 	{
-		console.log('out of stock :hidden')
+		//console.log('out of stock :hidden')
 		$('.ProductList .list-item .ProductActionAdd[style="display:none;"], .ProductList li .ProductActionAdd[style="display:none;"], .ProductList .list-item .ProductActionAdd[style="display: none;"], .ProductList li .ProductActionAdd[style="display: none;"]').parent().find('.ProductImage a').append( "<span class='SoldOutTag'>Sold Out</span>" );
 		
 	}
@@ -718,7 +738,7 @@ $(window).resize(function () {
 			$('.slideText').height(maxHslide).width(maxWslide);
 			
 			// Resets the product list images to center
-			//$('.ProductList .ProductImage img').centerImage();
+			// $('.ProductList .ProductImage img').centerImage();
 			
 			
 
@@ -761,7 +781,7 @@ if ($("html").hasClass("lt-ie9")) {
 	/*! http://mths.be/placeholder v2.0.8 by @mathias */
 	;(function(window, document, $) {
 
-		// Opera Mini v7 doesn’t support placeholder although its DOM seems to indicate so
+		// Opera Mini v7 doesnÃ¢â‚¬â„¢t support placeholder although its DOM seems to indicate so
 		var isOperaMini = Object.prototype.toString.call(window.operamini) == '[object OperaMini]';
 		var isInputSupported = 'placeholder' in document.createElement('input') && !isOperaMini;
 		var isTextareaSupported = 'placeholder' in document.createElement('textarea') && !isOperaMini;
